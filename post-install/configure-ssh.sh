19945810c172d1a968806c0b28d3da540c304332
matic <matic@assam.ijs.si>
2019-09-03 18:02:45 +0200

    modified configure-ssh.sh

--- post-install/configure-ssh.sh
+++ post-install/configure-ssh.sh
@@ -3,22 +3,49 @@
 #
 # make ssh configure file for user ... 
 #
-if [ $# -ne 2 ]; then
-    echo "Usage:
-    $0 username hostname"
-    exit 1
-fi
+userinput() {
+    echo "Please input your $1"
+    read reply 
+    echo "The $1 is $reply is this correct [y|n]"
+    read answer
+    if test "$answer" != "y"; then
+	echo "The reply $reply was wrong, exiting ..."
+	exit 1 
+    fi
+} 
 
-user=$1
-hostname=$2
+userinput username
+user=$reply
+userinput hostname 
+host=$reply
+
+
+# define where key is stored here (default $HOME/.ssh/)
+pathtokey="$HOME/.ssh"
+
+echo "Generating ssh-key in $pathtokey"
+
+mkdir -p $pathtokey
+
+nsckey="$pathtokey/id_rsa_nsc"
+
+ssh-keygen -t rsa -f $nsckey
 
 cat <<EOF > mysshconfig.txt
 Host nsc
- HostName $hostname 
+ HostName $host 
  User $user
- ForwardX11 yes
- 
+ IdentityFile $nsckey.pub
 EOF
+
+echo "BEWARE: you will have to input your password twice" 
+
+scp $nsckey.pub nsc:~/ 
+
+ssh -t nsc 'cat $HOME/id_rsa_nsc.pub >> $HOME/.ssh/authorized_keys ; rm $HOME/id_rsa_nsc.pub'
+
 cat mysshconfig.txt >> $HOME/.ssh/config
-echo "Accses $hostname by typing: 
+rm mysshconfig.txt
+
+echo "Please access the JSI cluster by typing: 
 ssh nsc"
